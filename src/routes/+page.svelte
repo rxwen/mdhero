da38834372b10bb71c4f1bba17c703f3db57b9bb
rxwen <rx.wen218@gmail.com>
2026-06-24 19:31:45 +0800

    fix: center markdown content with toc sidebar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -835,15 +835,15 @@
         maxWidth={$settings.maxWidth}
       />
     {:else if rawMode}
-      <main class="content-main">
+      <main class="content-main" class:toc-spaced={$tocVisible && $tocEntries.length > 0}>
         <pre
           class="raw-source"
           style="font-size: {$settings.fontSize}px; line-height: {$settings.lineHeight}; max-width: {$settings.maxWidth}px;"
         ><code>{$docStore.content}</code></pre>
       </main>
     {:else}
-      <FrontmatterBar />
-      <main class="content-main">
+      <main class="content-main" class:toc-spaced={$tocVisible && $tocEntries.length > 0}>
+        <FrontmatterBar />
         <MarkdownRenderer
           html={$docStore.renderedHtml}
           onImageClick={(src, all, idx) => { lightboxImages = all; lightboxIndex = idx; lightboxVisible = true; }}
@@ -910,6 +910,11 @@
 
   .content-main {
     padding-bottom: 4rem;
+    transition: padding-left 0.15s ease;
+  }
+
+  .content-main.toc-spaced {
+    padding-left: 240px;
   }
 
   .raw-source {
